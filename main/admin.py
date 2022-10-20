968a4c234b1aecb4f39f037f57ddb101afc6984d
Ykyki4 <jimmypantalones@gmail.com>
2022-10-20 16:05:20 +0300

    Add models and registred they on admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Student, Course
 
-# Register your models here.
+
+class CourseStudentsInline(admin.TabularInline):
+    model = Course.students.through
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    raw_id_fields = ['courses']
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [
+        CourseStudentsInline
+    ]
--- main/models.py
+++ main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField("Заголовок курса", max_length=200)
+    language = models.CharField("Язык программирования", max_length=50)
+    direction = models.CharField("Направление курса", max_length=100)
+    certification = models.CharField("Информация об сертификате", max_length=250)
+    description = models.TextField("Описание курса")
+    price = models.IntegerField("Стоимость курса")
+    hours = models.IntegerField("Сколько часов курс займёт")
+    lections = models.IntegerField("Количество лекций в курсе")
+    skill = models.CharField("Необходимый уровень знаний", max_length=50)
+    when_start = models.DateTimeField("Начало курса")
+    when_end = models.DateTimeField("Конец курса")
+
+    def __str__(self):
+        return self.title
+
+class Student(AbstractBaseUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True, max_length=150)
+    password = models.CharField(max_length=20)
+    Avatar = models.ImageField()
+    age = models.IntegerField()
+    courses = models.ManyToManyField(Course, related_name="students")
+
+    USERNAME_FIELD = "email"
+
