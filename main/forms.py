f6d28267e7e6f1c31ef20be64c562271d13eaf45
Ykyki4 <jimmypantalones@gmail.com>
2022-10-26 18:31:10 +0300

    Separated forms

--- main/forms.py
+++ main/forms.py
@@ -18,7 +18,23 @@ class RequestForm(forms.ModelForm):
             'first_name': forms.TextInput(attrs={'class': 'cons__input', 'placeholder': "Ім'я"}),
             'last_name': forms.TextInput(attrs={'class': 'cons__input', 'placeholder': 'Прізвище'}),
             'phone': forms.TextInput(attrs={'class': 'cons__input', 'placeholder':'Телефон'}),
-            'email': forms.EmailInput(attrs={'class': 'cons__input'}),
+            'email': forms.EmailInput(attrs={'class': 'cons__input', 'placeholder': 'Email'}),
             'course': forms.Select(attrs={'class': 'cons__input'}),
             'comment': forms.Textarea(attrs={'class': 'cons__input'})
         }
+class ConsForm(forms.ModelForm):
+    class Meta:
+        model = Request
+        fields = ('first_name', 'last_name', 'phone', 'email')
+        labels = {
+            'first_name': "Ім'я",
+            'last_name': 'Прізвище',
+            'phone': 'Телефон',
+            'email': 'E-mail',
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'cons__input', 'placeholder': "Ім'я"}),
+            'last_name': forms.TextInput(attrs={'class': 'cons__input', 'placeholder': 'Прізвище'}),
+            'phone': forms.TextInput(attrs={'class': 'cons__input', 'placeholder':'Телефон'}),
+            'email': forms.EmailInput(attrs={'class': 'cons__input', 'placeholder': 'Email'}),
+            }
